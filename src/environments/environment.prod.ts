6fdb166459e2e5797e35ed10c36d86eff001d099
Prahllad <royprahllad3@gmail.com>
2019-10-12 21:31:09 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  APP_DOMAIN: 'https://first-angular1-app1.herokuapp.com/',
+  APP_DOMAIN: 'first-angular1-app1.herokuapp.com',
   API: 'https://quizpi.herokuapp.com/api/v1',
   SECRET: 'c5796a2ae795a3f3bb3c9d13b814ad357957c19c4e0637abe39f9aefb7e92940'
 };
