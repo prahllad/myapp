d4fa41be39748440e42d0e014697ef1b4a40ca64
prahllad roy <prahllad.roy@venturepact.com>
2019-10-12 19:30:52 +0530

    new project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   APP_DOMAIN: 'localhost',
-  API: 'http://localhost:3001/api/v1',
+  API: 'https://quizpi.herokuapp.com/api/v1',
   SECRET: 'c5796a2ae795a3f3bb3c9d13b814ad357957c19c4e0637abe39f9aefb7e92940'
 };
